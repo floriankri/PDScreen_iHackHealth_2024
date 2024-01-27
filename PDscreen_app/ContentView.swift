a689197778729bcbfc24af6531c60a753a87b2e5
Florian Kristof <fk17091999@gmail.com>
2024-01-26 20:10:28 -0800

    Initial Commit

---
+++ PDscreen_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PDscreen_app
+//
+//  Created by Florian Kristof on 26.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PDscreen_app/PDscreen_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDscreen_appApp.swift
+//  PDscreen_app
+//
+//  Created by Florian Kristof on 26.01.24.
+//
+
+import SwiftUI
+
+@main
+struct PDscreen_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
