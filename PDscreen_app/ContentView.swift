9ae6cf1952650a24eebaca1bb917471e4ee6cc24
Florian Kristof <fk17091999@gmail.com>
2024-01-26 21:07:27 -0800

    Add counter

--- PDscreen_app/ContentView.swift
+++ PDscreen_app/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count: Int=0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("We are PDscreen!")
         }
         .padding()
+        
+        VStack(alignment: .center, content: {
+            Text("current count: \(count)")
+            Button(action: {
+                self.count += 1
+            }){
+                Text("Increment")
+            }
+        })
     }
 }
 
