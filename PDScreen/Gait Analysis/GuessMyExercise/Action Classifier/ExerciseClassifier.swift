24ad760390d348af27872666ea709b2b069de6df
Florian Kristof <fk17091999@gmail.com>
2024-02-04 16:56:51 -0800

    add Gait analysis not functional

---
+++ PDScreen/Gait Analysis/GuessMyExercise/Action Classifier/ExerciseClassifier.swift
@@ -0,0 +1,12 @@
+//
+//  ExerciseClassifier.swift
+//  Guess My Exercise
+//
+//  Created by Florian Kristof on 02.02.24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
+
+class ExerciseClassifier {
+}
